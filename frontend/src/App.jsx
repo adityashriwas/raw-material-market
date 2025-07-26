af4cad80a2636f880111416c409c8a24839ca0a3
Aditya Shriwas <adityashriwas09@gmail.com>
2025-07-26 13:05:13 +0530

    configured shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-      <div >
-        <p className='bg-amber-400 '>chandrika</p>
-      </div>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
